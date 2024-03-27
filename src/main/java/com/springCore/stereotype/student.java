299cb94b6a45afad82024caf58c7cb47aa50da61
Ankit0607 <ankit.mishra06072000@gmail.com>
2024-03-27 21:17:44 +0530

    How to invoke static method , variables and creating object | Spring Expression Language

--- src/main/java/com/springCore/stereotype/student.java
+++ src/main/java/com/springCore/stereotype/student.java
@@ -17,17 +17,21 @@ public class student {
 	@Value("#{list1}")
 	private List<String> address;
 
+	@Value("#{T(java.lang.Math).sqrt(144)}")
+	private double z;
+
+	@Value("#{T(java.lang.Math).PI}")
+	private double e;
+
+	@Value("#{ new java.lang.String('Ankit Mishra')}")
+	private String name1;
+
 	public List<String> getAddress() {
 		return address;
 	}
 
 	public void setAddress(List<String> address) {
 		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return "student [name=" + name + ", city=" + city + "]";
 	}
 
 	public String getName() {
@@ -45,4 +49,35 @@ public class student {
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public double getE() {
+		return e;
+	}
+
+	public void setE(double e) {
+		this.e = e;
+	}
+
+	public String getName1() {
+		return name1;
+	}
+
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+
+	@Override
+	public String toString() {
+		return "student [name=" + name + ", city=" + city + ", address=" + address + ", z=" + z + ", e=" + e
+				+ ", name1=" + name1 + "]";
+	}
+
 }
